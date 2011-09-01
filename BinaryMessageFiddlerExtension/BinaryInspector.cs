e7d5c1c8684a848220b4f37acd95beba181a42b0
Will Fuqua <wafuqua@gmail.com>
2011-08-31 21:55:14 -0400

    First pass - combine editable into main

--- BinaryMessageFiddlerExtension/BinaryInspector.cs
+++ BinaryMessageFiddlerExtension/BinaryInspector.cs
@@ -14,18 +14,29 @@ namespace BinaryMessageFiddlerExtension
 {
     public class BinaryInspector : Inspector2
     {
-        private XmlTreeView _myControl;
-        private byte[] _entityBody;
+        private Control container;
+        private XmlTreeView viewControl;
+        private TextBox editControl;
+
+        private byte[] binaryContent;
 
         private static readonly XmlDictionary _wcfBinaryDictionary = WcfBinaryDictionary.CreateWcfBinaryDictionary();
         private static Logger log = new Logger(true);
+        private bool isReadOnly;
 
         public override void AddToTab(TabPage o)
         {
-            _myControl = new XmlTreeView();
             o.Text = "WCF Binary XML";
-            o.Controls.Add(_myControl);
-            o.Controls[0].Dock = DockStyle.Fill;
+
+            viewControl = new XmlTreeView();
+            viewControl.BackColor = CONFIG.colorDisabledEdit;
+            viewControl.Dock = DockStyle.Fill;
+
+            editControl = new TextBox { Multiline = true, ScrollBars = ScrollBars.Vertical };
+            editControl.TextChanged += TextChanged;
+            editControl.Dock = DockStyle.Fill;
+            
+            container = o;
         }
 
         #region IBaseInspector2 Properties
@@ -35,67 +46,112 @@ namespace BinaryMessageFiddlerExtension
             return 1;
         }
 
-        public bool bDirty
-        {
-            get { return false; }
-        }
+        public bool bDirty { get; set; }
 
         public bool bReadOnly
         {
-            get { return true; }
-            set { }
+            get { return isReadOnly; }
+            set 
+            {
+                isReadOnly = value;
+                ChangeWidget();
+            }
+        }
+
+        private void ChangeWidget()
+        {
+            Control widget = isReadOnly ? viewControl : editControl as Control;
+            container.Controls.Clear();
+            container.Controls.Add(widget);
         }
 
         public void Clear()
         {
-            _myControl.Clear();
+            viewControl.Clear();
+            editControl.Clear();
+            bDirty = false;
+            bReadOnly = true;
         }
 
         public byte[] body
         {
-            get { return _entityBody; }
-            set { UpdateView(value); }
+            get { return binaryContent; }
+            set 
+            {
+                binaryContent = value;
+                UpdateContent(); 
+            }
         }
 
         #endregion
 
-        private void UpdateView(byte[] bytes)
+        private void UpdateContent()
         {
-            _entityBody = bytes;
-
             try
             {
-                XmlDocument document = GetWcfBinaryMessageAsXml(_entityBody);
+                XmlDocument document = GetWcfBinaryMessageAsXml(binaryContent);
 
                 if (document == null || document.ChildNodes.Count == 0)
+                {
                     this.Clear();
+                }
                 else
-                    _myControl.LoadXml(document);
+                {
+                    viewControl.LoadXml(document);
+                    editControl.Text = document.OuterXml;
+                }
             }
             catch (XmlException ex)
             {
                 log.LogString(ex.ToString());
-                _myControl.DisplayError(ex.Message);
+                viewControl.DisplayError(ex.Message);
             }
         }
 
         private XmlDocument GetWcfBinaryMessageAsXml(byte[] encodedMessage)
         {
             XmlDocument document = new XmlDocument();
-            using (var reader = CreateReaderForMessage(encodedMessage))
+            
+            XmlDictionaryReader reader = XmlDictionaryReader.CreateBinaryReader(
+                encodedMessage,
+                0,
+                encodedMessage.Length,
+                _wcfBinaryDictionary,
+                XmlDictionaryReaderQuotas.Max);
+
+            using (reader)
             {
                 document.Load(reader);
             }
+
             return document;
         }
 
-        private XmlDictionaryReader CreateReaderForMessage(byte[] encodedMessage)
+        public void TextChanged(object sender, EventArgs e)
         {
-            return XmlDictionaryReader.CreateBinaryReader(encodedMessage,
-                                                          0, 
-                                                          encodedMessage.Length,
-                                                          _wcfBinaryDictionary,
-                                                          XmlDictionaryReaderQuotas.Max);
+            XmlDocument document = new XmlDocument();
+            bDirty = true;
+            try
+            {
+                document.LoadXml(editControl.Text);
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    XmlDictionaryWriter binaryWriter = XmlDictionaryWriter.CreateBinaryWriter(ms);
+                    document.WriteContentTo(binaryWriter);
+                    binaryWriter.Flush();
+                    ms.Position = 0;
+                    binaryContent = new byte[Int32.Parse(ms.Length.ToString())];
+                    ms.Read(binaryContent, 0, Int32.Parse(ms.Length.ToString()));
+                    ms.Flush();
+                }
+            }
+            catch (XmlException ex)
+            {
+                log.LogString("An XML error occured, reverting to plain text");
+                System.Text.UTF8Encoding encoding = new System.Text.UTF8Encoding();
+                binaryContent = encoding.GetBytes(editControl.Text);
+            }
         }
     }
 }
